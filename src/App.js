836504b4e23ea64b69098ae5413b2c0b4ee9ac78
yunseongyeol <yso920616@naver.com>
2022-01-03 00:29:47 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h2>Lets develop managementdd system!</h2>
+    </div>
+  );
+}
+
+export default App;
