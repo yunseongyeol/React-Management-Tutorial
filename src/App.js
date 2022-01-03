82a96e57d2d044b62a9b1211952145ae85206463
yunseongyeol <yso920616@naver.com>
2022-01-03 23:29:25 +0900

    apply material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles} from '@material-ui/core/styles';
+import { mergeClasses } from '@material-ui/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [{
   'id' : 1,
@@ -29,7 +48,19 @@ const customers = [{
 
 function App() {
   return (
-    <div>
+    <Paper className={mergeClasses.root}>
+      <Table className={mergeClasses.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
 {
   customers.map(c => {
     return (
@@ -45,8 +76,10 @@ function App() {
     )
   })
 }
-</div>
+</TableBody>
+</Table>
+</Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
