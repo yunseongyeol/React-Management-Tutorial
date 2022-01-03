7d4ef773b4255b66a1921a32d849cb579c24a17f
yunseongyeol <yso920616@naver.com>
2022-01-03 23:00:01 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import Customers from './components/Customer';
+import Customer from './components/Customer';
 import './App.css';
 
-const customer = {
-  'name' : '홍길동',
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '다시마',
   'birthday' : '123456',
   'gender' : '남자',
   'job' : '대학생' 
-}
+},
+{
+'id' : 2,
+'image' : 'https://placeimg.com/64/64/2',
+'name' : '홍길동',
+'birthday' : '123456',
+'gender' : '남자',
+'job' : '대학생' 
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '너구리',
+  'birthday' : '123456',
+  'gender' : '남자',
+  'job' : '대학생' 
+  },
+]
 
 function App() {
   return (
-    <Customers
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+{
+  customers.map(c => {
+    return (
+      <Customer
+        key={c.id}
+        id={c.id}
+        image={c.image}
+        name={c.name}
+        birthday={c.name}
+        gender={c.gender}
+        job={c.job}
+      />
+    )
+  })
+}
+</div>
   );
 }
 
